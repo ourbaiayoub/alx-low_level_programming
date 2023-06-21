7b190b543673765bd0748bce0e796c5eabb74fb6
ourbaiayoub <ourbaiayoub@gmail.com>
2023-06-20 18:05:02 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
 #include "main.h"
-
-
 /**
  * main - print _putchar
  * is written by mbah
@@ -18,5 +16,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0);	
+return (0);
 }
